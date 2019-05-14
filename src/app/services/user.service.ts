4b20dbb7579cd2b21a0d370f3f18d8a5e4ac2f03
gsr9 <gsr9@alu.ua.es>
2019-05-14 19:04:19 +0200

    Refactor user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,14 +2,14 @@ import {Injectable} from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable()
-export class UserService{
-    
-    constructor(public afDB: AngularFireDatabase){
+export class UserService {
+
+    constructor(public afDB: AngularFireDatabase) {
     }
 
-    public getFruits(){
-        return this.afDB.list('/users').valueChanges(); 
-        //Esta función devolverá todos los datos que tengamos en el apartado fruits, en nuestra base de datos
+    public getUsers() {
+        return this.afDB.list('/users').valueChanges();
+        // Esta función devolverá todos los datos que tengamos en el apartado fruits, en nuestra base de datos
     }
 
 }
