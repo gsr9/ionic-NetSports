7c1d6046946b57ac2b9bff4943dc9f120c355c6d
unknown <jgl58@alu.ua.es>
2019-05-18 10:44:35 +0200

    Creado componente comentario

--- src/app/modules/publication/publication.module.ts
+++ src/app/modules/publication/publication.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { PublicationPage } from './publication.page';
+import { CommentsComponent} from './comments/comments.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [PublicationPage]
+  declarations: [PublicationPage,CommentsComponent]
 })
 export class PublicationPageModule {}
