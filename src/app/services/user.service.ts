f158db03d231d14484186e536cefdf8155196213
gsr9 <gsr9@alu.ua.es>
2019-05-20 10:25:16 +0200

    Added popup to show result and button to go back

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { User } from '../shared/models/User.model';
 
 @Injectable()
 export class UserService {
@@ -10,6 +11,18 @@ export class UserService {
     public getUsers() {
         return this.afDB.list('/users').valueChanges();
         // Esta función devolverá todos los datos que tengamos en el apartado fruits, en nuestra base de datos
+    }
+
+    public getUserByEmail(email: string) {
+        // return this.afDB.list('users', ref =>
+        //     ref.orderByChild('email').equalTo(email)
+        // ).valueChanges();
+       return this.afDB.list('users', ref =>
+            ref.orderByChild('email').equalTo(email)).snapshotChanges();
+    }
+
+    public updateUser(key: string, user: User) {
+        return this.afDB.object('/users/' + key).update(user);
     }
 
 }
