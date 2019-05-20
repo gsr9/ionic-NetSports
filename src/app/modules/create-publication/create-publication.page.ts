592ab7b013fe420d3bb5f2c9b373eadc3edb3aba
unknown <jgl58@alu.ua.es>
2019-05-20 20:23:18 +0200

    Cambiado muro

--- src/app/modules/create-publication/create-publication.page.ts
+++ src/app/modules/create-publication/create-publication.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PublicacionesService} from '../../services/publicaciones.service'
+import { Storage } from '@ionic/storage';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-create-publication',
   templateUrl: './create-publication.page.html',
@@ -11,14 +13,17 @@ export class CreatePublicationPage implements OnInit {
     title: '',
     description: ''
   };
-  constructor(public service:PublicacionesService) { }
+  constructor(public service:PublicacionesService,private storage: Storage,private router: Router) { }
 
   ngOnInit() {
   }
 
   logForm(form) {
-    this.service.setPublicaciones(this.todo.title,this.todo.description,"","0")
-    console.log(form.value)
+    this.storage.get('user').then((val: User) => {
+      this.service.setPublicaciones(this.todo.title,this.todo.description,val.username,"0")
+      this.router.navigate(['/tabs/wall'])
+    });
+
   }
 
 }
